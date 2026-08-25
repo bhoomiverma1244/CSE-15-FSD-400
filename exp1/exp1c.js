84825988d5c48650ee6bf80155a1a80b93653681
bhoomiverma1244 <bhoomiverma7581@gmail.com>
2026-08-25 11:50:52 +0530

    EXP 1 AND 2

---
+++ exp1/exp1c.js
@@ -0,0 +1,12 @@
+console.log("1.start");
+setTimeout(()=>{
+    console.log("2.setTimeout");
+},0);
+
+setImmediate(()=>{
+    console.log("3.setImmediate");
+});
+process.nextTick(()=>{
+    console.log("4.nextTick");
+
+});
---
+++ exp2.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+// cREATE a file
+fs.writeFileSync("demo.txt", "Hello, this is my file.");
+
+// READ the file
+let data = fs.readFileSync("demo.txt", "utf8");
+console.log("File content:", data);
+
+// UPDATE the file
+fs.appendFileSync("demo.txt", "\nThis is updated content.");
+
+data = fs.readFileSync("demo.txt", "utf8");
+console.log("After update:", data);
+
+// DELETE the file
+fs.unlinkSync("demo.txt");
+
+console.log("File deleted successfully.");
